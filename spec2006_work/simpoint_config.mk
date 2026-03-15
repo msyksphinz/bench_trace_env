e5395fb626f68d59557d6210c38ebc2abc7cd9d0
msyksphinz-self <msyksphinz.dev@gmail.com>
2026-03-15 17:58:28 +0900

    Update INTERVAL=20M

--- spec2006_work/simpoint_config.mk
+++ spec2006_work/simpoint_config.mk
@@ -1,4 +1,4 @@
 # SimPoint parameters (single source of truth for Makefile and scripts).
 # Scripts can read defaults via: make -s -C spec2006_work print-simpoint-config
-SIMPOINT_INTERVAL ?= 1000000
+SIMPOINT_INTERVAL ?= 20000000
 SIMPOINT_K ?= 30
